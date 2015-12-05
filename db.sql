81a392f80d028049c5577a2151cf4bbbfec3262d
Evan Warren <warrevan@users.noreply.github.com>
2015-12-05 16:05:13 -0500

    added db.sql

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `members` (
+  `memberID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `active` int(1) NOT NULL DEFAULT '0',
+  `resetToken` varchar(255) DEFAULT NULL,
+  `resetComplete` varchar(3) DEFAULT 'No',
+  PRIMARY KEY (`memberID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
