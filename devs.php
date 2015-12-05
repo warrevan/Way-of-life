69f9e3dee8dae2004076eded40dac99b2c39a4bf
Evan Warren <warrevan@users.noreply.github.com>
2015-12-05 16:06:43 -0500

    added devs.php

---
+++ devs.php
@@ -0,0 +1,33 @@
+<head>
+	<title>Developers</title>
+	<link type="text/css" rel="stylesheet" href="style/main.css" >
+        <link rel="shortcut icon" href="logo.png">
+</head>
+<body >
+<div class="top">
+    <a href="index.php"><img src="images/logo.png" style="margin-left:42%; margin-right:auto; height:100%;"></a>
+</div>
+<div class="main" style="background-color:#BDBDBD; margin:0 -.6% 0 -.6%; padding-bottom:10%;">
+<br>
+<div class="container" style="margin:5% 33% 3% 33%; padding:2.5%; background-color:white;">
+     <div class="justin">
+          <img style="float:right;" src="">
+          <h3>Justin Hazzard</h3>
+          <h4>Project Manager, Head of Design</h4>
+     </div>
+     <hr>
+     <div class="evan">
+          <img style="float:right;" src="">
+          <h3>Evan Warren</h3>
+          <h4>Head of Web Development</h4>
+     </div>
+</div>
+</div>
+<div class="bottom">
+     <a href="memberpage.php"><h6 style="display:inline;">My Account</h6></a>
+     <a href="help.php"><h6 style="display:inline; margin-left:5%;">Help</h6></a>
+     <a href="devs.php"><h6 style="display:inline;">Developers</h6></a>
+     <a href="sinfo.php"><h6 style="display:inline;">Site Info</h6></a>
+     <a href="translate.php"><h6 style="display:inline;">Translate</h6></a>
+</div>
+</body>
